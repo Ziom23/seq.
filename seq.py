8bc53102062dfc69d47470d5e44b9a8c0b32bfe9
Ziom23 <82481563+Ziom23@users.noreply.github.com>
2024-11-03 10:54:57 +0000

    Create seq.py

---
+++ seq.py
@@ -0,0 +1,17 @@
+def z():
+    if input=="zj":
+        print(0)
+        print(1)
+def j():
+    if input=="jz":
+        print(1)
+        print(0)
+a=0
+b=1
+while 0==0:
+    for j in range(1,2) or range(9,10)[:10]:
+        print(a,b,b,a)
+    for j in range(3,4) or range(7,8)[:10]:
+        print(b,a,a,b)
+    for j in range(5,6)[:10]:
+        print(a,a,a,b)
